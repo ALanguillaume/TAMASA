ce08256ff0fd9e2791103bb2a40ac31eb2c4da13
Languillaume, Antoine <antoine.languillaume@wur.nl>
2019-12-18 10:16:50 +0100

    update tree repo

--- scripts/build_report.R
+++ scripts/build_report.R
@@ -3,5 +3,6 @@ unlink("./rmd/_main.Rmd")
 setwd("C:/projects/TAMASA/rmd/")
 bookdown::render_book(input = "index.Rmd", 
                       output_dir = here::here("./docs/"))
-unlink("./rmd/_bookdown_files")
+unlink("./_bookdown_files", recursive = TRUE)
 setwd(here::here())
+
